534e01b52a81ee9004ee3a188adc52005a46dfba
Sean Murphy <sean@iamseanmurphy.com>
2025-04-22 08:19:08 +0000

    6.5.6

--- chatbot/includes/openai/admin/settings.php
+++ chatbot/includes/openai/admin/settings.php
@@ -49,6 +49,9 @@
         <div class="<?php esc_attr_e( 'mb-3','wpbot');?>">
             <label for="<?php esc_attr_e( 'max_tokens','wpbot');?>" id="<?php esc_attr_e( 'openai_engines','wpbot');?>" class="<?php esc_attr_e( 'form-label','wpbot');?>"><?php esc_html_e( 'OpenAI Model','wpbot');?></label>
             <select class="<?php esc_attr_e( 'form-select','wpbot');?>" aria-label="Default select example" name="openai_engines" id="<?php esc_attr_e( 'openai_engines','wpbot');?>">
+                <option value="<?php esc_attr_e( 'gpt-4.1-mini','wpbot'); ?>" <?php echo ((get_option( 'openai_engines') == 'gpt-4.1-mini') ? esc_attr('selected') : '') ; ?>><?php esc_html_e( 'GPT-4.1-Mini','wpbot');?></option>
+                <option value="<?php esc_attr_e( 'gpt-4.1-nano','wpbot'); ?>" <?php echo ((get_option( 'openai_engines') == 'gpt-4.1-nano') ? esc_attr('selected') : '') ; ?>><?php esc_html_e( 'GPT-4.1-Nano','wpbot');?></option>
+                <option value="<?php esc_attr_e( 'gpt-4.1','wpbot'); ?>" <?php echo ((get_option( 'openai_engines') == 'gpt-4.1') ? esc_attr('selected') : '') ; ?>><?php esc_html_e( 'GPT-4.1','wpbot');?></option>
                 <option value="<?php esc_attr_e( 'gpt-4o-mini','wpbot'); ?>" <?php echo ((get_option( 'openai_engines') == 'gpt-4o-mini') ? esc_attr('selected') : '') ; ?>><?php esc_html_e( 'GPT-4o-Mini','wpbot');?></option>
                 <option value="<?php esc_attr_e( 'gpt-4o','wpbot'); ?>" <?php echo ((get_option( 'openai_engines') == 'gpt-4o') ? esc_attr('selected') : '') ; ?>><?php esc_html_e( 'GPT-4o','wpbot');?></option>
                 <option value="<?php esc_attr_e( 'gpt-4-turbo','wpbot'); ?>" <?php echo ((get_option( 'openai_engines') == 'gpt-4-turbo') ? esc_attr('selected') : '') ; ?>><?php esc_html_e( 'gpt-4-turbo','wpbot');?></option>
--- chatbot/readme.txt
+++ chatbot/readme.txt
@@ -4,7 +4,7 @@ Donate link: https://www.wpbot.pro/
 Tags: chatbot, chatgpt, live chat, AI, live support
 Requires at least: 4.6
 Tested up to: 6.8
-Stable tag: 6.5.5
+Stable tag: 6.5.6
 Requires PHP: 5.6
 License: GPLv2 or later
 License URI: https://www.wpbot.pro/
@@ -16,6 +16,8 @@ AI ChatBot for WordPress - Automated 24/7 Live Chat Customer Support. NATIVE, Le
 WPBot is an easy to use, Native, No coding required, <strong>AI ChatBot for WordPress</strong> websites to provide <strong>automated Live Chat Support</strong>, <strong>Lead Generation</strong> and collecting information from your users. Best Free ChatBot plugin for WordPress websites to provide <strong>automated 24/7 customer support</strong>.
 
 This ChatBot can be also be powered by <strong>OpenAI ChatGPT</strong>, <strong>DialogFlow</strong>, or simply use the <strong>built-in features</strong> to provide Live Support, Lead Generation and collect data without <strong>any extra cost</strong>. Own and Manage this Native WordPress ChatBot directly from your WordPress Dashboard for improving live customer support efficiency and user conversions.
+
+> 🚀 EASTER SALE! Use DISCOUNT code <strong>sunday</strong> & <strong>UPGRADE to WPBot Pro</strong>
 
 > 🚀 <strong> for Advanced Features [Upgrade to WPBot Pro](https://www.wpbot.pro/) now!
 > 🚀 [Support, Bug Report, Feature Requests](https://www.wpbot.pro/free-support/) | [ChatBot for WordPress](https://www.wpbot.pro/) Demo
@@ -390,6 +392,9 @@ Images, GIFs and Youtube Videos are supprted in the pro version. Pro version als
 13. WPBot Conversational Form Builder
 
 == Changelog ==
+
+= 6.5.6 =
+# Added new AI models
 
 = 6.5.5 =
 # Easter Sale
